9df0812ec4dfe05fb3b218701c20f0c4d145da12
David Wu <david.wuu@gmail.com>
2014-07-01 15:27:52 -0700

    dyn_arr

--- dynamic_array.rb
+++ dynamic_array.rb
@@ -11,28 +11,45 @@ class DynamicArray < Array
 	def push(ele)
 		@size += 1
 		@first += 1
+		if(@first >= @arr_size-1)
+			@first -= @arr_size
+		end
 		self[@first] = ele
 		check_resize
+	end
+
+	def pop
+		@size -= 1
+		@first -= 1
+		if(@first <= -(@arr_size-1))
+			@first += @arr_size
+		end
+		check_resize
+		self[@first+1]
 	end
 
 	def unshift(ele)
 		@size += 1
 		@last -= 1
+		if @last < -(@arr_size-1)
+			@last += @arr_size
+		end
 		self[@last] = ele
 		check_resize
 	end
 
+	def shift
+		@size -= 1
+		@last += 1
+		if @last > (@arr_size-1)
+			@last -= @arr_size
+		end
+		check_resize
+		self[@last-1]
+	end
+
 	private
 	def check_resize
-
-		p self
-
-		#prevent @last from getting to big
-		if @last < (-1) * @arr_size
-			@last += @arr_size
-			p 'redo last'
-		end
-
 		if @size >= @arr_size
 			p 'resize! O(n)'
 			@arr_size *= 2
@@ -47,7 +64,21 @@ class DynamicArray < Array
 	end
 end
 
-da = DynamicArray.new(3)
+da = DynamicArray.new(2)
 (0..12).each do |val|
 	da.unshift(val)
+	p da.shift
 end
+(0..12).each do |val|
+	da.push(val)
+	p da.pop
+end
+(0..12).each do |val|
+	da.push(val)
+	p da.shift
+end
+(0..12).each do |val|
+	da.unshift(val)
+	p da.pop
+end
+
